5d741db3bb2c5de7f26452e457212a0935907091
sepideh-biglarizadeh <sepideh.biglarizadeh00@gmail.com>
2023-01-14 21:40:08 +0330

    reference method - relationship beetwen collections

---
+++ relation.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/relation")
+  .then(() => console.log("connected to mongodb"))
+  .catch(() => console.log("could not connect to mongodb"));
+
+const Book = mongoose.model(
+  "Book",
+  new mongoose.Schema({
+    title: String,
+    pages: Number,
+  })
+);
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+    },
+  })
+);
+
+async function createUser(first_name, last_name, book_id) {
+  const user = new User({
+    first_name,
+    last_name,
+    book: book_id,
+  });
+  const result = await user.save();
+  console.log(result);
+}
+
+async function createBook(title, pages) {
+  const book = new Book({
+    title,
+    pages,
+  });
+  const result = await book.save();
+  console.log(result);
+}
+
+async function getUsers() {
+  const users = await User.find().populate('book','title pages -_id');
+  console.log(users);
+}
+
+// createBook("Node.js programming", 100);
+// createUser('test','test123','63c2ee5d82dd03e223cdeb6a');
+getUsers();
